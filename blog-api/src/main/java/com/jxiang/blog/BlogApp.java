1839c08aa4c6899247324f450691a4988d574fe4
Jxiang2 <xjyhenry@gmail.com>
2022-10-16 15:31:46 -0400

    init admin app

--- blog-api/src/main/java/com/jxiang/blog/BlogApp.java
+++ blog-api/src/main/java/com/jxiang/blog/BlogApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlogApiApplication {
+public class BlogApp {
 
   public static void main(String[] args) {
-    SpringApplication.run(BlogApiApplication.class, args);
+    SpringApplication.run(BlogApp.class, args);
   }
 
 }
